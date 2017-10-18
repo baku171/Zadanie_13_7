782266b69f3e74d31b3748cbbcc1c36609bde77c
baku171 <pbak@esri.pl>
2017-10-18 13:37:55 +0200

    Zadanie 13.7

---
+++ program.js
@@ -0,0 +1,14 @@
+var fs = require('fs');
+
+fs.readdir('./', 'utf-8', function(err, data){
+    console.log(data);
+    fs.writeFile('./tekst.txt', data, 'utf-8', function(err) {
+        if (err) throw err;
+        console.log('Zapisano');
+        fs.readFile('./tekst.txt', 'utf-8', function(err, data) {
+            console.log('Dane po zapisie'.blue)
+            console.log(data);
+        });
+    });
+});
+
