e4bf85f69c53fe72838e28c8df7e9c3edb085dae
baku171 <pbak@esri.pl>
2017-10-18 13:41:44 +0200

    Zadanie 13.7

--- program.js
+++ program.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 
 fs.readdir('./', 'utf-8', function(err, data){
     console.log(data);
-    fs.writeFile('./tekst.txt', data, 'utf-8', function(err) {
+    fs.writeFile('./tekst.txt', data.join('\n'), 'utf-8', function(err) {
         if (err) throw err;
         console.log('Zapisano');
         fs.readFile('./tekst.txt', 'utf-8', function(err, data) {
